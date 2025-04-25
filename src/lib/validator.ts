7727c3ae4b8aa4f6b63bd732eb75876563b3778a
Richard RAKOTOMAVO <darirarakotomavo@example.com>
2025-04-25 15:42:21 +0300

    modification 01

--- src/lib/validator.ts
+++ src/lib/validator.ts
@@ -2,13 +2,40 @@ import { z } from "zod";
 import { formatNumberWithDecimal } from "./utils";
 
 // Common
+const MongoId = z
+  .string()
+  .regex(/^[0-9a-fA-F]{24}$/, { message: 'Invalid MongoDB ID' })
 const Price = (field: string) =>
   z.coerce
     .number()
     .refine(
       (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(value)),
       `${field} must have exactly two decimal places (e.g., 49.99)`
-    );
+  );
+/*     export const ReviewInputSchema = z.object({
+      product: MongoId,
+      user: MongoId,
+      isVerifiedPurchase: z.boolean(),
+      title: z.string().min(1, "Title is required"),
+      comment: z.string().min(1, "Comment is required"),
+      rating: z.coerce
+        .number()
+        .int()
+        .min(1, "Rating must be at least 1")
+        .max(5, "Rating must be at most 5"),
+    }); */
+    export const ReviewInputSchema = z.object({
+      product: MongoId,
+      user: MongoId,
+      isVerifiedPurchase: z.boolean(),
+      title: z.string().min(1, "Title is required"),
+      comment: z.string().min(1, "Comment is required"),
+      rating: z.coerce
+        .number()
+        .int()
+        .min(1, "Rating must be at least 1")
+        .max(5, "Rating must be at most 5"),
+    });
 export const ProductInputSchema = z.object({
   name: z.string().min(3, "Name must be at least 3 characters"),
   slug: z.string().min(3, "Slug must be at least 3 characters"),
@@ -43,6 +70,12 @@ export const ProductInputSchema = z.object({
     .int()
     .nonnegative("Number of sales must be a non-negative number"),
 });
+/* export const ProductUpdateSchema = ProductInputSchema.extend({
+  _id: z.string(),
+}); */
+export const ProductUpdateSchema = ProductInputSchema.extend({
+  _id: z.string(),
+});
 
    // Order Item
 export const OrderItemSchema = z.object({
@@ -64,7 +97,54 @@ export const OrderItemSchema = z.object({
   size: z.string().optional(),
   color: z.string().optional(),
 });
-// Cart
+ export const ShippingAddressSchema = z.object({
+  fullName: z.string().min(1, "Full name is required"),
+  street: z.string().min(1, "Address is required"),
+  city: z.string().min(1, "City is required"),
+  postalCode: z.string().min(1, "Postal code is required"),
+  province: z.string().min(1, "Province is required"),
+  phone: z.string().min(1, "Phone number is required"),
+  country: z.string().min(1, "Country is required"),
+});  
+// Order
+
+export const OrderInputSchema = z.object({
+  user: z.union([
+    MongoId,
+    z.object({
+      name: z.string(),
+      email: z.string().email(),
+    }),
+  ]),
+  items: z
+    .array(OrderItemSchema)
+    .min(1, 'Order must contain at least one item'),
+  shippingAddress: ShippingAddressSchema,
+  paymentMethod: z.string().min(1, 'Payment method is required'),
+  paymentResult: z
+    .object({
+      id: z.string(),
+      status: z.string(),
+      email_address: z.string(),
+      pricePaid: z.string(),
+    })
+    .optional(),
+  itemsPrice: Price('Items price'),
+  shippingPrice: Price('Shipping price'),
+  taxPrice: Price('Tax price'),
+  totalPrice: Price('Total price'),
+  expectedDeliveryDate: z
+    .date()
+    .refine(
+      (value) => value > new Date(),
+      'Expected delivery date must be in the future'
+    ),
+  isDelivered: z.boolean().default(false),
+  deliveredAt: z.date().optional(),
+  isPaid: z.boolean().default(false),
+  paidAt: z.date().optional(),
+})
+/* // Cart
 export const ShippingAddressSchema = z.object({
   fullName: z.string().min(1, "Full name is required"),
   street: z.string().min(1, "Address is required"),
@@ -73,8 +153,8 @@ export const ShippingAddressSchema = z.object({
   province: z.string().min(1, "Province is required"),
   phone: z.string().min(1, "Phone number is required"),
   country: z.string().min(1, "Country is required"),
-});
-
+}); */
+//lesson 11
 export const CartSchema = z.object({
   items: z
     .array(OrderItemSchema)
@@ -129,6 +209,88 @@ export const UserSignUpSchema = UserSignInSchema.extend({
   message: "Passwords don't match",
   path: ["confirmPassword"],
 });
+export const UserNameSchema = z.object({
+  name: UserName,
+});
+export const CarouselSchema = z.object({
+  title: z.string().min(1, 'title is required'),
+  url: z.string().min(1, 'url is required'),
+  image: z.string().min(1, 'image is required'),
+  buttonCaption: z.string().min(1, 'buttonCaption is required'),
+});
+//setting
+export const SiteLanguageSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  code: z.string().min(1, "Code is required"),
+});
+
+export const SettingInputSchema = z.object({
+  // PROMPT: create fields
+  // codeium, based on the mongoose schema for settings
+  common: z.object({
+    pageSize: z.coerce
+      .number()
+      .min(1, "Page size must be at least 1")
+      .default(9),
+    isMaintenanceMode: z.boolean().default(false),
+    freeShippingMinPrice: z.coerce
+      .number()
+      .min(0, "Free shipping min price must be at least 0")
+      .default(0),
+    defaultTheme: z
+      .string()
+      .min(1, "Default theme is required")
+      .default("light"),
+    defaultColor: z
+      .string()
+      .min(1, "Default color is required")
+      .default("gold"),
+  }),
+  site: z.object({
+    name: z.string().min(1, "Name is required"),
+    logo: z.string().min(1, "logo is required"),
+    slogan: z.string().min(1, "Slogan is required"),
+    description: z.string().min(1, "Description is required"),
+    keywords: z.string().min(1, "Keywords is required"),
+    url: z.string().min(1, "Url is required"),
+    email: z.string().min(1, "Email is required"),
+    phone: z.string().min(1, "Phone is required"),
+    author: z.string().min(1, "Author is required"),
+    copyright: z.string().min(1, "Copyright is required"),
+    address: z.string().min(1, "Address is required"),
+  }),
+  carousels: z
+    .array(CarouselSchema)
+    .min(1, "At least one language is required"),
+  availableLanguages: z
+    .array(SiteLanguageSchema)
+    .min(1, "At least one language is required"),
+
+  /*  availableLanguages: z
+    .array(SiteLanguageSchema)
+    .min(1, "At least one language is required"),
+
+  carousels: z
+    .array(CarouselSchema)
+    .min(1, "At least one language is required"),
+  defaultLanguage: z.string().min(1, "Language is required"),
+  availableCurrencies: z
+    .array(SiteCurrencySchema)
+    .min(1, "At least one currency is required"),
+  defaultCurrency: z.string().min(1, "Currency is required"),
+  availablePaymentMethods: z
+    .array(PaymentMethodSchema)
+    .min(1, "At least one payment method is required"),
+  defaultPaymentMethod: z.string().min(1, "Payment method is required"),
+  availableDeliveryDates: z
+    .array(DeliveryDateSchema)
+    .min(1, "At least one delivery date is required"),
+  defaultDeliveryDate: z.string().min(1, "Delivery date is required"), */
+});
+
+
+
+
 
 
 
@@ -617,7 +779,7 @@ eckout/checkout-footer.tsx
 import { APP_NAME } from '@/lib/constants'
 import Link from 'next/link'
 import React from 'react'
-import { ShippingAddressSchema } from './validator';
+import { ShippingAddressSchema, CarouselSchema, SiteLanguageSchema } from './validator';
 import { model } from 'mongoose';
 import { shippingAddress } from '@/types';
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,125 +1,23 @@
 import {
-  CartSchema,
-  OrderItemSchema,
-  ProductInputSchema,
-  ShippingAddressSchema,
-} from "@/lib/validator";
-import { z } from "zod";
-
-export type IProductInput = z.infer<typeof ProductInputSchema>;
-
-export type Data = {
-  products: IProductInput[];
-  headerMenus: {
-    name: string;
-    href: string;
-  }[];
-  carousels: {
-    image: string;
-    url: string;
-    title: string;
-    buttonCaption: string;
-    isPublished: boolean;
-  }[];
-};
-export type OrderItem = z.infer<typeof OrderItemSchema>;
-export type Cart = z.infer<typeof CartSchema>;
-export type ShippingAddress = z.infer<typeof ShippingAddressSchema>;
-
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// lesson 5 connect to mongodb types/index
-/*  ```
-
-8. create types/index.ts
-
-   import { ProductInputSchema } from "@/lib/validator";
-import { z } from "zod";
-
-   export type IProductInput = z.infer<typeof ProductInputSchema>
-
-10 update types/index.tsx
-   10. types/index.ts
-
-    ```ts
-    import { ProductInputSchema } from '@/lib/validator'
-    import { z } from 'zod'
-
-    export type IProductInput = z.infer<typeof ProductInputSchema>
-    export type Data = {
-      products: IProductInput[]
-      headerMenus: {
-        name: string
-        href: string
-      }[]
-      carousels: {
-        image: string
-        url: string
-        title: string
-        buttonCaption: string
-        isPublished: boolean
-      }[]
-    }
-    ```
-    */
-
-//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-//lesson 11 types/index.tsx
-/* 5. types/index.ts
-
- import {
-  CartSchema,
-  OrderItemSchema,
-  ProductInputSchema,
-} from "@/lib/validator";
-export type OrderItem = z.infer<typeof OrderItemSchema>;
-export type Cart = z.infer<typeof CartSchema>;
- */
-
-/* XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-types/index.tsx main github final
-
-import {
   CarouselSchema,
   CartSchema,
-  DeliveryDateSchema,
-  OrderInputSchema,
   OrderItemSchema,
-  PaymentMethodSchema,
   ProductInputSchema,
-  ReviewInputSchema,
-  SettingInputSchema,
   ShippingAddressSchema,
-  SiteCurrencySchema,
-  SiteLanguageSchema,
   UserInputSchema,
   UserNameSchema,
   UserSignInSchema,
   UserSignUpSchema,
-  WebPageInputSchema,
 } from "@/lib/validator";
 import { z } from "zod";
-import { ShippingAddressSchema } from './../lib/validator';
+import { SettingInputSchema } from '@/lib/validator';
 
-export type IReviewInput = z.infer<typeof ReviewInputSchema>;
-export type IReviewDetails = IReviewInput & {
-  _id: string;
-  createdAt: string;
-  user: {
-    name: string;
-  };
-};
 export type IProductInput = z.infer<typeof ProductInputSchema>;
 
 export type Data = {
   settings: ISettingInput[];
-  webPages: IWebPageInput[];
-  users: IUserInput[];
   products: IProductInput[];
-  reviews: {
-    title: string;
-    rating: number;
-    comment: string;
-  }[];
+  users: IUserInput[];
   headerMenus: {
     name: string;
     href: string;
@@ -131,39 +29,30 @@ export type Data = {
     buttonCaption: string;
     isPublished: boolean;
   }[];
-};
-// Order
-export type IOrderInput = z.infer<typeof OrderInputSchema>;
-export type IOrderList = IOrderInput & {
-  _id: string;
-  user: {
-    name: string;
-    email: string;
-  };
-  createdAt: Date;
 };
 export type OrderItem = z.infer<typeof OrderItemSchema>;
 export type Cart = z.infer<typeof CartSchema>;
 export type ShippingAddress = z.infer<typeof ShippingAddressSchema>;
 
 // user
-export type IUserInput = z.infer<typeof UserInputSchema>;
-export type IUserSignIn = z.infer<typeof UserSignInSchema>;
-export type IUserSignUp = z.infer<typeof UserSignUpSchema>;
-export type IUserName = z.infer<typeof UserNameSchema>;
-
-// webpage
-export type IWebPageInput = z.infer<typeof WebPageInputSchema>;
-
-// setting
+export type IUserInput = z.infer<typeof UserInputSchema>
+export type IUserSignIn = z.infer<typeof UserSignInSchema>
+export type IUserSignUp = z.infer<typeof UserSignUpSchema>
+export type IUserName = z.infer<typeof UserNameSchema>
+//setting
+export type ISettingInput = z.infer<typeof SettingInputSchema>
 export type ICarousel = z.infer<typeof CarouselSchema>;
-export type ISettingInput = z.infer<typeof SettingInputSchema>;
-export type ClientSetting = ISettingInput & {
-  currency: string;
-};
-export type SiteLanguage = z.infer<typeof SiteLanguageSchema>;
-export type SiteCurrency = z.infer<typeof SiteCurrencySchema>;
-export type PaymentMethod = z.infer<typeof PaymentMethodSchema>;
-export type DeliveryDate = z.infer<typeof DeliveryDateSchema>;
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
- */
+
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+/* export type Data = {
+  settings: ISettingInput[]
+  webPages: IWebPageInput[]
+  users: IUserInput[]
+  products: IProductInput[]
+  reviews: {
+    title: string
+    rating: number
+    comment: string
+  }[] */
